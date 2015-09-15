4cce9bdce00e9b4ba916fc725356b54fe6e56c23
Jose Torres & Deron Johnson <student@epicodus.com>
2015-09-15 15:17:20 -0700

    adds age related discount tests and functions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ function Ticket(movieName, movieTime, viewerAge, originalPrice) {
   this.viewerAge = viewerAge;
   this.originalPrice = originalPrice;
 }
+
+function calculatePrice(ticket) {
+  if (ticket.viewerAge >=65) {
+    return ticket.originalPrice -=5;
+  } else if (ticket.movieTime <= 1700) {
+    return ticket.originalPrice -= 2;
+  } else {
+    return ticket.originalPrice;
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -10,11 +10,17 @@ describe('Ticket', function() {
 
 describe('calculatePrice', function() {
   it("discounts a ticket if a movie is before 1700 by $2", function() {
-    var testTicket = new Ticket("The Last Mustache", 1900, 30, 10);
-    expect(testTicket.movieName).to.equal("The Last Mustache");
-    expect(testTicket.movieTime).to.equal(1900);
-    expect(testTicket.viewerAge).to.equal(30);
-    expect(testTicket.originalPrice).to.equal(10);
-    expect(calculatePrice(testTicket.originalPrice)).to.equal(8);
+    var testTicket = new Ticket("The Last Mustache", 1200, 30, 10);
+    expect(calculatePrice(testTicket)).to.equal(8);
+  });
+
+  it("if a viewer is 65 or older, it discounts a ticket by $5", function() {
+    var testTicket = new Ticket("The End of the Road", 1400, 67, 10);
+    expect(calculatePrice(testTicket)).to.equal(5);
+  });
+
+  it("if a viewer is under the age of 12, it discouts a ticket by $5", function() {
+    var testTicket = new Ticket("The Last Scoop of IceCream", 1300, 9, 10);
+    expect(calculatePrice(testTicket)).to.equal(5);
   });
 });
