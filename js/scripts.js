d405bfb286766bb9335ee5592b1967c8a3f7cfb9
Jose Torres & Deron Johnson <student@epicodus.com>
2015-09-15 16:16:54 -0700

    Add jQuery to take form input and return a ticket price

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function Ticket(movieName, movieTime, viewerAge, originalPrice) {
 }
 
 function calculatePrice(ticket) {
+  ticket.movieTime *= 100;
   if (ticket.viewerAge >=65 || ticket.viewerAge <= 12) {
     return ticket.originalPrice -=5;
   } else if (ticket.movieTime <= 1700) {
@@ -14,3 +15,21 @@ function calculatePrice(ticket) {
     return ticket.originalPrice;
   }
 }
+
+$(document).ready(function() {
+  $("form#new-ticket").submit(function(event) {
+    event.preventDefault();
+
+    var movieName = $("select#movie-name").val();
+    var movieTime = parseInt($("input#movie-time").val());
+    var viewerAge = parseInt($("input#viewer-age").val());
+    var originalPrice = 10;
+
+    var newTicket = new Ticket(movieName, movieTime, viewerAge, originalPrice);
+    debugger;
+    var ticketPrice = calculatePrice(newTicket);
+
+    $("#ticket-price").text(ticketPrice);
+    $("#results").show();
+  });
+});
