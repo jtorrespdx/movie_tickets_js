23367bdeb029636cafbd8ddae4d9d65f9e0133b1
Jose Torres & Deron Johnson <student@epicodus.com>
2015-09-15 14:14:19 -0700

    modifies first test

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,8 @@
 describe('ticket', function() {
   it("creates a new ticket item with its properties", function() {
-    var testTicket = new Ticket("The Last Mustache","7:00", 18);
+    var testTicket = new Ticket("The Last Mustache", "7:00", 30);
     expect(testTicket.movieName).to.equal("The Last Mustache");
     expect(testTicket.movieTime).to.equal("7:00");
-    expect(testTicket.originalPrice).to.equal(18);
+    expect(testTicket.viewerAge).to.equal(30);
   });
+});
